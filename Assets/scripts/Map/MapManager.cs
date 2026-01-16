2e47215eb0f903c5ca39e66c2575af805b8e8555
ajlares <angel.osorno@uniat.edu.mx>
2026-01-16 14:09:58 -0600

    singleton

--- Assets/scripts/Map/MapManager.cs
+++ Assets/scripts/Map/MapManager.cs
@@ -5,6 +5,13 @@ public class MapManager : MonoBehaviour
     public MapManager Instance;
     private void Awake()
     {
-
+        if (Instance != null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 }
