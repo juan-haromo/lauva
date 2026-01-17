3a6fae935cdecddaa84fbad6e3b1312b65842d6f
MrMangoDS <angel.osorno@uniat.edu.mx>
2026-01-17 02:11:47 -0600

    commit

---
+++ Assets/scripts/Animations/BriechAnimation.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BriechAnimation : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+
+    IEnumerator startAnimation()
+    {
+        // quitar movimiento
+        
+        
+        
+        
+        // movimiento mona chafa
+        
+        
+        
+        // ponerMovimiento
+        yield return null;
+    }
+}
